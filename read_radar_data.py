54d46a1f2cecf6fbf61f4b9939a1cc543776481d
arghasen10 <arghasen10@gmail.com>
2024-11-03 18:46:12 +0530

    feature computation

--- read_radar_data.py
+++ read_radar_data.py
@@ -2,7 +2,7 @@ import numpy  as np
 import glob
 import mmwave.dsp as dsp
 from mmwave.dataloader import DCA1000
-
+from helper import *
 
 mmwave_files = glob.glob('dataset/raw_data_day1/*.npy')
 
@@ -17,10 +17,13 @@ for file in mmwave_files:
             mean = radar_cube.mean(0)                 
             radar_cube = radar_cube - mean
             radar_cube = np.concatenate((radar_cube[0::3,...], radar_cube[1::3,...], radar_cube[2::3,...]), axis=1)
-            print(radar_cube.shape)
-        print(data.shape)
-        print(timeArr[1])
-    break
+            radar_cube = radar_cube.reshape(182, 3, 4, 256)
+            doppler_fft = dopplerFFT(radar_cube)
+            pcds = frame2pointcloud(doppler_fft)
 
-
-# with open('')
+            ## Feature 1 (shape: 256)
+            rangeOutput = np.transpose(np.absolute(radar_cube), (1,2,0,3)).sum(axis=(0,1,2))
+            ## Feature 2 (shape: 182, 256)
+            rangeDoppler = np.absolute(doppler_fft).sum(axis=(0,1))
+            ## feature 3 (1600,6)
+            pcds
